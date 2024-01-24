e11d877ec1ee05e0ae84d647c792b9163002a884
Willem van Heemstra <wvanheemstra@icloud.com>
2024-01-24 15:52:08 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+		
+	]
+});
 
 /** @type {import('vite').UserConfig} */
-const config = {
+/** const config = {
 	plugins: [sveltekit()]
 };
 
 export default config;
+*/
